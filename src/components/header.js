5de7d2347261423126872d8eeeb0e1f213bfff46
Ivan Shumilov <ivanshum@mustbefamily.com>
2024-05-08 12:30:05 +0200

    header fixes and changes

--- src/components/header.js
+++ src/components/header.js
@@ -16,7 +16,7 @@ const Header = ({ data, loc }) => {
   { slug: `contacts`, title: `contacts.title` }
   ];
   return (<>
-    <header className={`backdrop-blur-sm bg-black/50 py-[10px] lg:py-5 fixed top-0 w-full z-50`}>
+    <header className={`bg-black py-[10px] lg:py-5 fixed top-0 w-full z-[100]`}>
       <div className={`lg:container lg:mx-auto px-5 flex flex-row items-center`}>
         <div className={`w-2/12 lg:w-1/5 flex lg:justify-start justify-end order-2 lg:order-1`}>
           <button id={`menu`} className={`cursor-pointer h-[22px] w-[28px] rotate-0 ease-in-out duration-500 relative${isOpen ? ` open` : ``}`} onClick={() => setOpen(!isOpen)}>
@@ -41,7 +41,7 @@ const Header = ({ data, loc }) => {
       </div>
       <nav className={`transition-all duration-500 lg:mx-auto px-5 flex ${isOpen ? `pt-5 h-screen lg:container lg:pt-0 lg:h-[38px]` : `h-0`} flex-col items-center justify-start lg:flex-row lg:items-end lg:justify-center`}>
         {links.map((e) => (
-          <Link to={`/${e.slug}`} activeClassName={`underline`} className={`text-white transition-all duration-500 uppercase lg:px-5 font-bold text-xs ${isOpen ? `pointer-events-auto py-2 lg:py-0 opacity-100` : `pointer-events-none opacity-0`}`}>{t({ id: e.title })}</Link>
+          <Link to={`/${e.slug}`} activeClassName={`underline`} className={`text-white transition-all duration-500 uppercase lg:px-5 font-bold text-base lg:text-sm ${isOpen ? `pointer-events-auto py-2 lg:py-0 opacity-100` : `pointer-events-none opacity-0`}`}>{t({ id: e.title })}</Link>
         ))}
       </nav>
     </header>
