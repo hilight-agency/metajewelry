a80d494c42ec2068590adfe668d554142a4ea1e2
Ivan Shumilov <ivanshummi@gmail.com>
2024-06-24 15:49:45 +0200

    privacy policy translation

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -5,151 +5,162 @@ const PrivacyPolicyPage = () => {
   return (
     <>
       <GatsbySeo
-        title={`Privacy Policy`}
-        description={`highlights.mustbefamily.com website administration is obliged to maintain your privacy on the Internet. We pay great attention to securing the data you provided to us. Our privacy policy is based on&nbsp;<a href="https://ec.europa.eu/info/law/law-topic/data-protection_en" target="_blank" rel="noreferrer">the General Data Protection Regulation (GDPR) of the European Union.</a>The purposes, for which we collect your personal data are: improvement of our service, communication with visitors to this site, user data processing for the purpose of online sales, presenting the information requested by the user and for other actions listed below.`}
-        canonical={`https://highlights.mustbefamily.com/contacts`}
+        title={`Политика конфиденциальности`}
+        description={`Настоящий документ представляет собой правила использования данных интернет-пользователей, собираемых с использованием данного сайта`}
+        canonical={`https://highlights.mustbefamily.com/privacy-policy`}
         openGraph={{
-          url: "https://highlights.mustbefamily.com/contacts",
+          url: "https://highlights.mustbefamily.com/privacy-policy",
         }}
       />
       <div className={`bg-black text-white px-5 w-full pt-16 lg:pt-28 pb-5`}>
         <div className={`prose lg:prose-xl prose-invert max-w-5xl md:mx-auto`}>
-          <h1>Privacy Policy</h1>
+          <h1>Политика конфиденциальности.</h1>
           <p>
-            highlights.mustbefamily.com website administration is obliged to
-            maintain your privacy on the Internet. We pay great attention to
-            securing the data you provided to us. Our privacy policy is based
-            on&nbsp;
-            <a
-              href="https://ec.europa.eu/info/law/law-topic/data-protection_en"
-              target="_blank"
-              rel="noreferrer"
-            >
-              the General Data Protection Regulation (GDPR) of the European
-              Union.
-            </a>
-            The purposes, for which we collect your personal data are:
-            improvement of our service, communication with visitors to this
-            site, user data processing for the purpose of online sales,
-            presenting the information requested by the user and for other
-            actions listed below.
+            Настоящий документ &laquo;Политика конфиденциальности&raquo; (далее
+            по тексту &ndash; &laquo;Политика&raquo;) представляет собой правила
+            использования Касторским Ильей Вячеславовичем (далее &ndash;
+            &laquo;мы&raquo; и/или &laquo;Администрация&raquo;) данных
+            интернет-пользователей (далее &laquo;вы&raquo; и/или
+            &laquo;Пользователь&raquo;), собираемых с использованием сайта
+            https://highlights.mustbefamily.com (далее &ndash;
+            &laquo;Сайт&raquo;).
           </p>
-          <h2>Personal data storage and processing</h2>
+          <h2>1. Обрабатываемые данные</h2>
           <p>
-            We collect and process your personal data only with your willing
-            consent. With your permission, we can collect and process the
-            following data: name and surname, e-mail address, phone number, .
-            Collection and processing of your personal information is carried
-            out in accordance with the laws of the European Union and the
-            Turckey.
-          </p>
-          <h2>Data storage, alteration, and removal</h2>
-          <p>
-            The user, who has provided highlights.mustbefamily.com with their
-            personal data, has the right for their alteration and removal, as
-            well as the right to recall the agreement to data processing. Time,
-            during which your personal data will be stored is: time the personal
-            data is used that is necessary for the website&rsquo;s main
-            activity. After finishing with the processing of your personal data,
-            the website&rsquo;s administration will permanently delete it. To
-            access your personal data, you can contact the administration on:
-            highlightsjewelry@gmail.com. We will be able to pass your data to a
-            third party only with your willing consent. If the data was
-            transferred to a third party, which is not associated with our
-            organization, we cannot perform any changes to that data.
-          </p>
-          <h2>Processing of visiting technical data</h2>
-          <p>
-            Records of your IP address, time of visit, browser settings,
-            operational system and other technical information is saved in the
-            database when you visit highlights.mustbefamily.com. This data is
-            necessary for the correct display of the website&rsquo;s content. It
-            is impossible to identify the person of the visitor using this data.
-          </p>
-          <h2>Children&rsquo;s personal information</h2>
-          <p>
-            If you are a parent or a legal guardian of an underage child, and
-            you know that the child has provided us with their personal
-            information without your consent, please contact us on:
-            highlightsjewelry@gmail.com. It is forbidden to enter personal data
-            of underage users without the agreement of parents or lawful
-            guardians.
-          </p>
-          <h2>Cookies processing</h2>
-          <p>
-            We use cookie files for the correct display of the website&rsquo;s
-            content and for the convenience of browsing
-            highlights.mustbefamily.com. They are small files, that are stored
-            on your device. They help the website to remember information about
-            you, such as in which language you use the website and which pages
-            have you already opened. This information will be useful in the next
-            visit. Thanks to cookie files, the website&rsquo;s browsing becomes
-            much more convenient. You can learn more about these files&nbsp;
-            <a
-              href="https://en.wikipedia.org/wiki/HTTP_cookie"
-              target="_blank"
-              rel="noreferrer"
-            >
-              here
-            </a>
-            . You can set up cookies reception and blocking in your browser
-            yourself. Inability to receive cookie files may limit the
-            functionality of the website.
-          </p>
-          <h2>Processing of personal data by other services</h2>
-          <p>
-            This website uses third-party online services, which perform data
-            collecting, independent from us. Such services include: Google
-            Analytics , Yandex.Metrica.
+            1.1. Мы не осуществляем сбор ваших персональных данных с
+            использованием Сайта.
           </p>
           <p>
-            Data collected by these services may be provided to other services
-            within those organizations. They can use the data for advertising
-            personalization of their own advertising network. You can learn
-            about user agreements of those organizations on their websites. You
-            can also refuse their collection of your personal data. For example,
-            Google Analytics Opt-out Browser Add-on can be found&nbsp;
-            <a
-              href="https://tools.google.com/dlpage/gaoptout?hl=en"
-              target="_blank"
-              rel="noreferrer"
-            >
-              here
-            </a>
-            . We do not pass any personal data to other organizations or
-            services, which are not listed in this privacy policy. As an
-            exception, the collected data may be provided on a lawful request of
-            state authorities, that are authorized to request such information.
+            1.2. Все данные, собираемые на Сайте, предоставляются и принимаются
+            в обезличенной форме (далее &ndash; &laquo;Обезличенные
+            данные&raquo;).
           </p>
-          <h2>Links to other websites</h2>
           <p>
-            Our website highlights.mustbefamily.com may contain links to other
-            websites, which are not under our control. We are not responsible
-            for the content of these websites. We recommend you familiarize
-            yourself with the privacy policy of every website that you visit, if
-            such policy exists.
+            1.3. Обезличенные данные включают следующие сведения, которые не
+            позволяют вас идентифицировать:
           </p>
-          <h2>Changes to the privacy policy</h2>
           <p>
-            From time to time, our website highlights.mustbefamily.com may
-            update our privacy policy. We inform about any changes to the
-            privacy policy, placed on this webpage. We are monitoring any
-            changes in legislation, which is related to personal data in
-            European Union and Turckey. If you have entered any of your personal
-            data on our website, we will inform you about the changes in our
-            privacy policy. If your personal data, and more specifically, your
-            contact information was entered incorrectly, we will not be able to
-            contact you.
+            1.3.1. Информацию, которую вы предоставляете о себе самостоятельно с
+            использованием онлайн-форм и программных модулей Сайта, включая имя
+            или номер телефона и/или адрес электронной почты.
           </p>
-          <h2>Feedback and final clauses</h2>
           <p>
-            You can contact the administration of highlights.mustbefamily.com
-            regarding any questions related to privacy policy on:
-            highlightsjewelry@gmail.com, or by filling a contact form specified
-            in a corresponding section of this website. If you do not agree with
-            this privacy policy, you cannot use the services of
-            highlights.mustbefamily.com. In this case you should avoid visiting
-            our website.
+            1.3.2. Данные, которые передаются в обезличенном виде в
+            автоматическом режиме в зависимости от настроек используемого вами
+            программного обеспечения.
+          </p>
+          <p>
+            1.4. Администрация вправе устанавливать требования к составу
+            Обезличенных данных Пользователя, которые собираются использованием
+            Сайта.
+          </p>
+          <p>
+            1.5. Если определенная информация не помечена как обязательная, ее
+            предоставление или раскрытие осуществляется Пользователем на свое
+            усмотрение и по собственной инициативе.
+          </p>
+          <p>
+            1.6. Администрация не осуществляет проверку достоверности
+            предоставляемых данных и наличия у Пользователя необходимого
+            согласия на их обработку в соответствии с настоящей Политикой,
+            полагая, что Пользователь действует добросовестно, осмотрительно и
+            прилагает все необходимые усилия к поддержанию такой информации в
+            актуальном состоянии и получению всех необходимых согласий на ее
+            использование.
+          </p>
+          <p>
+            1.7. Вы осознаете и принимаете возможность использования на Сайте
+            программного обеспечения третьих лиц, в результате чего такие лица
+            могут получать и передавать указанные в п.1.3 данные в обезличенном
+            виде.
+          </p>
+          <p>
+            1.8. Состав и условия сбора обезличенных данных с использованием
+            программного обеспечения третьих лиц определяются непосредственно их
+            правообладателями и могут включать:
+          </p>
+          <ul>
+            <li>данные браузера (тип, версия, cookie);</li>
+            <li>данные устройства и место его положения;</li>
+            <li>
+              данные операционной системы (тип, версия, разрешение экрана);
+            </li>
+            <li>данные запроса (время, источник перехода, IP-адрес).</li>
+          </ul>
+          <p>
+            1.9. Администрация не несет ответственность за порядок использования
+            Обезличенных данных Пользователя третьими лицами.
+          </p>
+          <h2>2. Цели обработки данных</h2>
+          <p>2.1. Администрация использует данные в следующих целях:</p>
+          <p>2.1.1. Обработка поступающих запросов и связи с Пользователем;</p>
+          <p>
+            2.1.2. Информационное обслуживание, включая рассылку
+            рекламно-информационных материалов;
+          </p>
+          <p>
+            2.1.3. Проведение маркетинговых, статистических и иных исследований;
+          </p>
+          <p>2.1.4. Таргетирование рекламных материалов на Сайте.</p>
+          <h2>3. Требования к защите данных</h2>
+          <p>
+            3.1. Администрация осуществляет хранение данных и обеспечивает их
+            охрану от несанкционированного доступа и распространения в
+            соответствии с внутренними правилами и регламентами.
+          </p>
+          <p>
+            3.2. В отношении полученных данных сохраняется конфиденциальность,
+            за исключением случаев, когда они сделаны Пользователем
+            общедоступными, а также когда используемые на Сайте технологии и
+            программное обеспечение третьих лиц либо настройки используемого
+            Пользователем программного обеспечения предусматривают открытый
+            обмен с данными лицами и/или иными участниками и пользователями сети
+            Интернет.
+          </p>
+          <p>
+            3.3. В целях повышения качества работы Администрация вправе хранить
+            лог-файлы о действиях, совершенных Пользователем в рамках
+            использования Сайта в течение 1 (Одного) года.
+          </p>
+          <h2>4. Передача данных</h2>
+          <p>
+            4.1. Администрация вправе передать данные третьим лицам в следующих
+            случаях:
+          </p>
+          <ul>
+            <li>
+              Пользователь выразил свое согласие на такие действия, включая
+              случаи применения Пользователем настроек используемого
+              программного обеспечения, не ограничивающих предоставление
+              определенной информации;
+            </li>
+            <li>
+              Передача необходима в рамках использования Пользователем
+              функциональных возможностей Сайта;
+            </li>
+            <li>
+              Передача требуется в соответствии с целями обработки данных;
+            </li>
+            <li>
+              В связи с передачей Сайта во владение, пользование или
+              собственность такого третьего лица;
+            </li>
+            <li>
+              По запросу суда или иного уполномоченного государственного органа
+              в рамках установленной законодательством процедуры;
+            </li>
+            <li>
+              Для защиты прав и законных интересов Администрации в связи с
+              допущенными Пользователем нарушениями.
+            </li>
+          </ul>
+          <h2>5. Изменение Политики конфиденциальности</h2>
+          <p>
+            5.1. Настоящая Политика может быть изменена или прекращена
+            Администрацией в одностороннем порядке без предварительного
+            уведомления Пользователя. Новая редакция Политики вступает в силу с
+            момента ее размещения на Сайте, если иное не предусмотрено новой
+            редакцией Политики.
           </p>
         </div>
       </div>
